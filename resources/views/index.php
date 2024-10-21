574adff96ec5669a5753016e3eaf8716fc2cca4f
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-10-20 21:50:06 -0300

    feat: format_money helper creation

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,18 +2,17 @@
 <?php require 'includes/top-nav.php' ?>
 <?php require 'includes/header.php' ?>
 
-<main>
-    <div>
-        <h2 class="text-3xl leading-6 font-bold text-gray-900">Products</h2>
-        <ul>
-            <?php foreach($products as $product): ?>
-                <li>
-                    <a href="/product?id=<?= $product['id'] ?>">
-                        <?= $product['name'] ?> - R$<?= $product['price'] ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-    </div>
+<main class="mx-auto max-w-7xl py-6 px-8">
+    <h2 class="text-3xl leading-6 font-bold text-gray-900">Products</h2>
+    <ul>
+        <?php foreach($products as $product): ?>
+            <li>
+                <a href="/product?id=<?= $product['id'] ?>">
+                    <?= $product['name'] ?> - <?= format_money($product['price']) ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </main>
 
 <?php require 'includes/footer.php' ?>
