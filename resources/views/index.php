b09d932da7bf2b2a4fa881bc55c6b3cdf7af379f
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-10-20 21:45:07 -0300

    fix: indentation enhancement

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,20 +2,18 @@
 <?php require 'includes/top-nav.php' ?>
 <?php require 'includes/header.php' ?>
 
-    <main>
-        <div class="mx-auto max-w-7xl py-6 px-8">
-            <div>
-                <h2 class="text-3xl leading-6 font-bold text-gray-900">Products</h2>
-                <ul>
-                    <?php foreach($products as $product): ?>
-                        <li>
-                            <a href="/product?id=<?= $product['id'] ?>">
-                                <?= $product['name'] ?> - R$<?= $product['price'] ?>
-                            </a>
-                        </li>
-                    <?php endforeach; ?>
-            </div>
-        </div>
-    </main>
+<main>
+    <div>
+        <h2 class="text-3xl leading-6 font-bold text-gray-900">Products</h2>
+        <ul>
+            <?php foreach($products as $product): ?>
+                <li>
+                    <a href="/product?id=<?= $product['id'] ?>">
+                        <?= $product['name'] ?> - R$<?= $product['price'] ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+    </div>
+</main>
 
 <?php require 'includes/footer.php' ?>
